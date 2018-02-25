1923fa55dc434c9e56aecec4a0a175c6bdaab89c
ChangJoo Park <pcjpcj2@gmail.com>
2018-02-25 13:10:35 +0900

    Update API endpoint config

--- src/mixins/api.js
+++ src/mixins/api.js
@@ -1,23 +1,25 @@
 import axios from 'axios'
+import config from '../config'
 
 const http = axios.create({
+  baseURL: config.API_ENDPOINT,
   timeout: 1000
 })
 
 const fetchTasks = () => {
-  return http.get('api/tasks')
+  return http.get('tasks')
 }
 
 const createTask = (body) => {
-  return http.post('api/tasks', { body })
+  return http.post('tasks', { body })
 }
 
 const removeTask = ({ id }) => {
-  return http.delete(`api/task/${id}`)
+  return http.delete(`task/${id}`)
 }
 
 const toggleTaskCompleted = ({ id, isCompleted, body }) => {
-  return http.patch(`api/task/${id}`, { isCompleted, body })
+  return http.patch(`task/${id}`, { isCompleted, body })
 }
 export default {
   methods: {
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 module.exports = {
   devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:3000',
-        pathRewrite: {
-          '^/api': ''
-        },
-        changeOrigin: true
-      }
-    }
   }
 }
